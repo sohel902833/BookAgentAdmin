fdc698011dd9d5477a264717785f1873101cf89d
Md Sohrab Hossain <mdsohelranask6869@gmail.com>
2021-04-17 21:27:39 +0600

    First Message

--- app/src/main/java/com/sohel/bookagentadmin/MainActivity.java
+++ app/src/main/java/com/sohel/bookagentadmin/MainActivity.java
@@ -1,14 +1,83 @@
 package com.sohel.bookagentadmin;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.internal.Constants;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE=100;
+
+    private Button chooseImageButton;
+   private Uri ImageUri;
+    ArrayList imageList = new ArrayList();
+    private int upload_count = 0;
+    private ProgressDialog progressDialog;
+    ArrayList urlStrings;
+    private TextView alertTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        chooseImageButton=findViewById(R.id.choose_ImageButton);
+        chooseImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivityForResult(intent, PICK_IMAGE);
+            }
+        });
+
+
+
+
     }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                if (data.getClipData() != null) {
+                    int countClipData = data.getClipData().getItemCount();
+                    int currentImageSlect = 0;
+                    while (currentImageSlect < countClipData) {
+
+                        ImageUri = data.getClipData().getItemAt(currentImageSlect).getUri();
+                        imageList.add(ImageUri);
+                        currentImageSlect = currentImageSlect + 1;
+                    }
+                    uploadIntoFirebase(imageList);
+                } else {
+                    Toast.makeText(this, "Please Select Multiple Images", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+    }
+
+    private void uploadIntoFirebase(ArrayList imageList) {
+    }
+
+
 }
